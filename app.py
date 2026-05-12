52065ffcfeb615f23d1e253d4066afd5f66af0c6
marielpagayonan <pagayonanmariel@gmail.com>
2026-05-12 18:40:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with st.sidebar:
 if api_key:
     try:
         genai.configure(api_key=api_key)
-        model = genai.GenerativeModel('gemini-pro')
+        model = genai.GenerativeModel('gemini-1.5-flash')
 
         if "messages" not in st.session_state:
             st.session_state.messages = []
